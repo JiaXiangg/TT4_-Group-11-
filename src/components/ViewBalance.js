038da1cdf162058714d22be927ac38e672b93669
JingleFox <kwanjf@hotmail.com>
2021-05-07 11:06:43 +0800

    added view balance template

---
+++ src/components/ViewBalance.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function ViewBalance() {
+    return (
+        <div>
+            <table className = "view-balance">
+                <tr>
+                    <th>Transaction Balance</th>
+                    <th>Currency Type</th>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default ViewBalance
