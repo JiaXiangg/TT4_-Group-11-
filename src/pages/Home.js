73dbf5468ba514aa7687603a01bf84d66dde9d9e
chanys5 <yiksiong@hotmail.com>
2021-05-07 14:12:37 +0800

    Added user details and logout button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,41 @@ const Home = ({isLoggedIn,onSubmitSignOut, userData, setUserData}) =>{
 
 		<div>
 			{
-				isLoggedIn ? ( 
-				<h2>Welcome to E-Wallet</h2>
+				isLoggedIn ? (
+					<div className="container">
+					<header className="jumbotron">
+						<h3>
+							Welcome to E-Wallet
+						</h3>
+						<h4>Profile Details</h4>
+						<h5>
+							<p>
+								<strong>First Name:</strong> {userData.firstName}
+							</p>
+							<p>
+								<strong>Last Name:</strong> {userData.lastName}
+							</p>
+							<p>
+								<strong>Gender:</strong> {userData.gender}
+							</p>
+							<p>
+								<strong>Age:</strong> {userData.age}
+							</p>
+							<p>
+								<strong>Address:</strong> {userData.address}
+							</p>
+							<p>
+								<strong>Email:</strong> {userData.email}
+							</p>
+							<p>
+								<strong>NRIC:</strong> {userData.nric}
+							</p>
+							<p>
+								<strong>Phone Number:</strong> {userData.phoneNumber}
+							</p>
+						</h5>
+					</header>
+					</div>
 				) : (
 				<Redirect to ='/login'/>
 				 ) 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -63,65 +63,71 @@ const Login = ({isLoggedIn, setisLoggedIn, userData, setUserData}) => {
 
   }
 
-  return (
-		<div className="col-md-12">
-			<div className="card card-container">
-				<img
-					src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-					alt="profile-img"
-					className="profile-img-card"
-				/>
+  return ( (!isLoggedIn ? (<div className="col-md-12">
+                          <div className="card card-container">
+                            <img
+                              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                              alt="profile-img"
+                              className="profile-img-card"
+                            />
 
-				<Form onSubmit={handleLogin} ref={form}>
-					<div className="form-group">
-						<label htmlFor="username">
-							Username
-						</label>
-						<Input 
-							type="text"
-							className="form-control"
-							name="username"
-							value={username}
-							onChange={onChangeUsername}
-							validations={[required]}
-						/>
-					</div>
+                            <Form onSubmit={handleLogin} ref={form}>
+                              <div className="form-group">
+                                <label htmlFor="username">
+                                  Username
+                                </label>
+                                <Input 
+                                  type="text"
+                                  className="form-control"
+                                  name="username"
+                                  value={username}
+                                  onChange={onChangeUsername}
+                                  validations={[required]}
+                                />
+                              </div>
 
-					<div className="form-group">
-						<label htmlFor="password">
-							Password
-						</label>
-						<Input 
-							type="password"
-							className="form-control"
-							name="password"
-							value={password}
-							onChange={onChangePassword}
-							validations={[required]}
-						/>
-					</div>
-					
-					<div className="form-group">
-						<button className="btn btn-primary btn-block" disabled={loading}>
-							{loading && (
-								<span className="spinner-border spinner-border-sm"></span>
-							)}
-							<span>Login</span>
-						</button>
-					</div>
+                              <div className="form-group">
+                                <label htmlFor="password">
+                                  Password
+                                </label>
+                                <Input 
+                                  type="password"
+                                  className="form-control"
+                                  name="password"
+                                  value={password}
+                                  onChange={onChangePassword}
+                                  validations={[required]}
+                                />
+                              </div>
+                              
+                              <div className="form-group">
+                                <button className="btn btn-primary btn-block" disabled={loading}>
+                                  {loading && (
+                                    <span className="spinner-border spinner-border-sm"></span>
+                                  )}
+                                  <span>Login</span>
+                                </button>
+                              </div>
 
-					{message && (
-						<div className="form-group">
-							<div className="alert alert-danger" role="alert">
-								Credentials provided are invalid
-							</div>
-						</div>
-					)}
+                              {message && (
+                                <div className="form-group">
+                                  <div className="alert alert-danger" role="alert">
+                                    Credentials provided are invalid
+                                  </div>
+                                </div>
+                              )}
 
-					<CheckButton style={{display: "none"}} ref={checkBtn}/>
-				</Form>
-			</div>
-		</div>
+                              <CheckButton style={{display: "none"}} ref={checkBtn}/>
+                            </Form>
+                          </div>
+                        </div>) 
+                        : <div>
+                            <div>
+                            Already login. Do you want to log out?
+                            </div>
+                            <button className="btn btn-default" onClick={()=>{setisLoggedIn(false)}}> Logout </button>
+                          </div>)
+	
 	)
 }
 
