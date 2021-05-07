d445ee79a0cf8578489bc3939502fe0f75281aba
Jia Xiang <limjiaxiang94@yahoo.com.sg>
2021-05-07 12:16:16 +0800

    added / to <li>

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App(){
               <li><Link to='/home'>Home</Link></li>
               <li><Link to='/login'>View Account Details</Link></li>
               <li><Link to='/viewaccountdetails'>Login</Link></li>
-              <li><Link to='/viewtransactiondetails'>View Transaction Details</Link><li>
-              <li><Link to='/addtransaction'>Add Transaction </Link><li>
+              <li><Link to='/viewtransactiondetails'>View Transaction Details</Link></li>
+              <li><Link to='/addtransaction'>Add Transaction </Link></li>
             </ul>
 
             <Route exact path = '/home'>
