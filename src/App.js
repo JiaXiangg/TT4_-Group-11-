13921ef8d6493046b32686bc0ed8727643832b1e
Jia Xiang <limjiaxiang94@yahoo.com.sg>
2021-05-07 10:57:43 +0800

    edited app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, useLocation} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+
+  const [accountKey, setaccountKey] = useState("");
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  const onSubmitSignOut = (e) => {
+    AuthService.signout();
+    e.preventDefault();
+    setisLoggedIn(false)
+  }
+
+
+    return(
+
+      <div>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to='/home'>Home</Link></li>
+              <li><Link to='/login'>Login</Link></li>
+              <li><Link to='/viewaccountdetails'>Login</Link></li>
+            </ul>
+
+            <Route exact path = '/home'>
+              <Home isLoggedIn = { isLoggedIn } onSubmitSignOut = { onSubmitSignOut } accountkey ={accountKey} setaccountKey={setaccountKey} />
+            </Route>
+
+            <Route exact path = '/login'>
+              <Login isLoggedIn = { isLoggedIn } setisLoggedIn = { setisLoggedIn } />
+            </Route>
+
+            <PrivateRoute exact path = '/viewaccountdetails'>
+              <ViewAccountDetails accountkey ={ accountKey }/>
+            </PrivateRoute>
+
+          </div>
+        </Router>
+    
+
+{/*       {isLoggedIn ? ( 
+          <UserInformation isLoggedIn = {isLoggedIn} accountkey ={accountKey} setaccountKey={setaccountKey} />
+          ) : (
+          <p>Please Log in</p> ) }*/}
+      </div>
+
+    )
+
+};
 
 export default App;
